d1b19dfad7a527b8977b13726670b477b61a3ffc
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-03-19 11:06:18 -0700

    Skipped over task 4. Completed task 5

--- index.js
+++ index.js
@@ -263,9 +263,12 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(arr, num){
+  arr.splice([num], 1);
+  return arr.length;
 }
+
+console.log(removeArtist(artists, 0));
    
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
